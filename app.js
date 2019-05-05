2b5890cc5c78062f6f702d44814bc9faf53a302c
Andresrodart <arodartel1601@alumno.ipn.mx>
2019-05-05 08:16:26 -0500

    Curiosito

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+var cookieParser = require('cookie-parser');
 var createError = require('http-errors');
+var favicon = require('serve-favicon')
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
